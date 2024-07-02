4310e9e4cf61886a280477f3727ff9d42b166f20
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-02 13:42:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.getElementById('oauthButton').addEventListener('click', function() {
     // Replace with your actual OAuth flow logic
+
     // Example OAuth flow steps (simplified for demonstration):
+
     // Step 1: Redirect users to Google OAuth consent page
     const clientId = '919212619443-d2ck4cv25sfhvvg5n1rj82ob81h56362.apps.googleusercontent.com'; // Replace with your Google OAuth client ID
     const redirectUri = 'https://engineerr1983.github.io/hello-world-page/callback'; // Replace with your redirect URI
 
-    const authUrl = 'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&scope=https://www.googleapis.com/auth/gmail.compose';
+    const authUrl = `https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=https://www.googleapis.com/auth/gmail.compose`;
 
     window.location.href = authUrl;
 
