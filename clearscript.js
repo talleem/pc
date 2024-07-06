dd00c6b8a08adb2dc6070a4be5a7f5944fc3938f
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-06 06:57:17 -0700

    Create clearscript.js

---
+++ clearscript.js
@@ -0,0 +1,8 @@
+// Function to clear the input fields
+function clearFields() {
+    // Clear the email input
+    document.getElementById('receiverEmail').value = '';
+
+    // Clear the textarea
+    document.getElementById('messageBody').value = '';
+}
