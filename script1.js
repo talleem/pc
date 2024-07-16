65d38f96f5a750e8218dac424137ef8bcf324a73
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-16 14:01:06 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -51,7 +51,7 @@ function showMessage2(message2) {
     div2.style.border = '1px solid #b8daff';
     div2.style.padding = '10px';
     div2.style.margin = '10px';
-    div2.style.fontWeight = 'bold';
+    
     
     // Append the <div> to the document body or another element where you want to display the message
     document.body.appendChild(div2);
