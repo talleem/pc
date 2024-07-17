5f8e85ff971e0468df57fa299e0d1fc62998d888
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-17 08:54:01 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -28,7 +28,7 @@ function showMessage(message) {
     div.style.border = '1px solid #b8daff';
     div.style.padding = '10px';
     div.style.margin = '10px';
-    div.style.fontSize = '1.2em';
+    div.style.fontSize = '1.1em';
      div.style.width = 'fit-content';
     
     
