34885b71609083ffcea0d1c512924caf056d855e
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-21 07:09:32 -0700

    Update populatewithcontacts.js

--- populatewithcontacts.js
+++ populatewithcontacts.js
@@ -53,7 +53,8 @@ async function fetchContacts() {
     try {
         const response = await fetch(url, {
             headers: {
-                'Authorization': 'Bearer ${newAccessToken}',
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
             },
         });
 
