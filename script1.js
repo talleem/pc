993a96a6f886add7c8d6b7b130a9cfafe08b7e65
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-23 09:00:19 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -37,9 +37,24 @@ function showMessage(message) {
     return div;
    }
 
+//Open the browser with full screen
+function openFullscreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+    document.documentElement.msRequestFullscreen();
+  }
+}
+
+
 //function to open a new tab to Gmail
     function openGmail() {
       window.open('https://mail.google.com/', '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');
+    openFullscreen();
     }
 
 
