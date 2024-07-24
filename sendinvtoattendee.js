ed9c48409ea04ec18591f67d2be761a1801ffc59
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-24 09:06:22 -0700

    Update sendinvtoattendee.js

--- sendinvtoattendee.js
+++ sendinvtoattendee.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const saveButton = document.getElementById('sendinvit');
     const savedValue = document.getElementById('savedValue');
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_AUTH_DOMAIN",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-        appId: "YOUR_APP_ID"
-    };
+  // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU0ns9VzWBxbHOIgTR-Yb6g1aFbOQEWFA",
+  authDomain: "engineerr1983meet.firebaseapp.com",
+  projectId: "engineerr1983meet",
+  storageBucket: "engineerr1983meet.appspot.com",
+  messagingSenderId: "308801516934",
+  appId: "1:308801516934:web:1a3833be5e03dfbcd66807",
+  measurementId: "G-X22VZ2TVWT"
+};
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
     // Initialize Firestore
     const db = firebase.firestore();
