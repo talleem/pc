32b6fb5c56b9746c802b6cb2b18649df3fe38c5a
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-24 13:28:50 -0700

    Update sendinvtoattendee.js

--- sendinvtoattendee.js
+++ sendinvtoattendee.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const savedValue = document.getElementById('savedValue');
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
