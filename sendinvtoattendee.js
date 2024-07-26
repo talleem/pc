5626585b486fe4733046caf274d780b40fdcf076
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-07-26 10:54:56 -0700

    Update sendinvtoattendee.js

--- sendinvtoattendee.js
+++ sendinvtoattendee.js
@@ -70,3 +70,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const joinMeetingButton = document.getElementById('joinmeeting');
+    const savedValuesList = document.getElementById('savedValuesList');
+
+    // Function to check the list and enable/disable the button
+    function checkList() {
+        if (savedValuesList.children.length > 0) {
+            joinMeetingButton.disabled = true;
+        } else {
+            joinMeetingButton.disabled = false;
+        }
+    }
+});
