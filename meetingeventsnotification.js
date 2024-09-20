ec422690736ba6b40b7f2603bccca60c10a9a97c
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-09-20 17:55:34 +0300

    Update meetingeventsnotification.js

--- meetingeventsnotification.js
+++ meetingeventsnotification.js
@@ -64,7 +64,7 @@ function showNotification(event) {
         right: 10px;
         margin-bottom: 10px;
         padding: 10px;
-        width: 300px;
+        width: 350px;
         background-color: #f0f0f0;
         border: 1px solid #ccc;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
