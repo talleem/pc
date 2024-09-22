7aec4b738dfc8f72327ce85649741cd72376801e
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-09-22 20:00:22 +0300

    Update instavideo.js

--- instavideo.js
+++ instavideo.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(document.querySelectorAll('.instagram-container')); // Log to check
     // Function to handle mouse enter event
     function openInstaVideoInNewWindow(event) {
+         console.log("Mouse entered!"); // Check if this message logs
         // Get the link of the Instagram video from the hovered container
         const linkElement = event.currentTarget.querySelector('a');
         const videoUrl = linkElement ? linkElement.href : null;
@@ -12,7 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const windowHeight = 700; // Adjust height accordingly
             
             // Open new window
+             console.log("Opening window with URL:", videoUrl); // Check if videoUrl is valid
             window.open(videoUrl, '_blank', `width=${windowWidth},height=${windowHeight},scrollbars=no`);
+        }
+        else {
+             console.log("No valid URL found in the container.");
         }
     }
 
