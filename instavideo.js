8495c05e732708de00db9599c35ffcca56c9cc4e
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-09-26 17:52:05 +0300

    Update instavideo.js

--- instavideo.js
+++ instavideo.js
@@ -5,11 +5,11 @@ function handleMouseEnter() {
     containers.forEach(container => {
         if (container !== this) {
             // Scale down the other containers to 1/6 of their original size
-            container.style.transform = 'scale(0.1667)'; // 1/6
+            container.style.transform = 'scale(0.0667)'; // 1/6
             container.style.transition = 'transform 0.3s'; // Smooth transition
         } else {
             // Restore the size of the hovered container
-            container.style.transform = 'scale(1)'; // Original size
+            container.style.transform = 'scale(2)'; // Original size
             container.style.transition = 'transform 0.3s'; // Smooth transition
         }
     });
