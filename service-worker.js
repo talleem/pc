792b9b22f04e7e94dbf6b33325f0ff8b28ae77ad
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-03 22:05:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('hello-world-cache').then(cache => {
+            return cache.addAll([
+                '/hello-world-page/',
+                '/hello-world-page/index.html',
+                '/hello-world-page/style.css',
+                '/hello-world-page/script.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
