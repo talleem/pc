416f7e35b6be471318d2389c9338d46ab4d8bb04
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-06 18:15:45 +0300

    Update index_AR.js

--- index_AR.js
+++ index_AR.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
         // Check if localStorage has the Arabic page setting after the button click
         if (localStorage.getItem("arpage") === "ar") {
             // Replace text with Arabic equivalent
-            document.querySelector("h3").innerHTML = "<b>هذا تطبيق كوليج تالم <font color='#FF0000'> ملاحظة: لا يزال قيد التطوير </font></b>";
+            document.querySelector("h3").innerHTML = "<b>تطبيق معهد (تعلم) الالكتروني <font color='#FF0000'> ملاحظة: لا يزال قيد التطوير </font></b>";
             
             // Replace the application entry link with Arabic
             document.querySelector("a[href='https://engineerr1983.github.io/hello-world-page/loginscreen.html'] h3").innerHTML = "<b>ادخل التطبيق من هنا</b>";
             
             // Replace the second <h4> (index 1) with the Arabic equivalent
-            document.querySelectorAll("h4")[1].innerHTML = "<b>يحتوي التطبيق على غرفة اجتماعات مدعومة بواسطة Google Meet / منشورات الكلية وسير المحاضرين (السير الذاتية والفيديوهات) / المحاضرات وأي ملفات أخرى يتم تحميلها يدويًا</b>";
+            document.querySelectorAll("h4")[1].innerHTML = "<b>تطبيق المعهد يحتوي على غرفة شات اجتماعات مدعوم من غوغل / منشورات المعهد ومحاضرات وسير ذاتية للمدرسين / صفحة محتوى المعهد تحتوي على المحاضرات وأية ملفات أخرى</b>";
         }
     });
 });
