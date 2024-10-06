e43c215c16769be90d354cf11cd313120d159c00
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-06 18:57:47 +0300

    Update index_AR.js

--- index_AR.js
+++ index_AR.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Set the localStorage variable when the button is clicked and check its value
     document.getElementById("topButton").addEventListener("click", function() {
         localStorage.setItem("arpage", "ar");
         
-        // Check if localStorage has the Arabic page setting after the button click..
+        // Check if localStorage has the Arabic page setting after the button click
         if (localStorage.getItem("arpage") === "ar") {
             // Replace text with Arabic equivalent
-            document.querySelector("h2").innerHTML = "<b>تطبيق معهد (تعلم) الالكتروني <font color='#FF0000'> ملاحظة: لا يزال قيد التطوير </font></b>";
+            const arabicH2 = "<b>تطبيق معهد (تعلم) الالكتروني <font color='#FF0000'> ملاحظة: لا يزال قيد التطوير </font></b>";
+            document.querySelector("h2").innerHTML = arabicH2;
+            document.querySelector("h2").classList.add("arabic"); // Add Arabic class
             
             // Replace the application entry link with Arabic
-            document.querySelector("a[href='https://engineerr1983.github.io/hello-world-page/loginscreen.html'] h3").innerHTML = "<b>ادخل التطبيق من هنا</b>";
-            
+            const arabicLink = "<b>ادخل التطبيق من هنا</b>";
+            document.querySelector("a[href='https://engineerr1983.github.io/hello-world-page/loginscreen.html'] h3").innerHTML = arabicLink;
+            document.querySelector("a[href='https://engineerr1983.github.io/hello-world-page/loginscreen.html'] h3").classList.add("arabic"); // Add Arabic class
+
             // Replace the second <h4> (index 1) with the Arabic equivalent
-            document.querySelectorAll("h4")[1].innerHTML = "<b>تطبيق المعهد يحتوي على غرفة شات اجتماعات مدعوم من غوغل / منشورات المعهد ومحاضرات وسير ذاتية للمدرسين / صفحة محتوى المعهد تحتوي على المحاضرات وأية ملفات أخرى</b>";
+            const arabicH4 = "<b>تطبيق المعهد يحتوي على غرفة شات اجتماعات مدعوم من غوغل / منشورات المعهد ومحاضرات وسير ذاتية للمدرسين / صفحة محتوى المعهد تحتوي على المحاضرات وأية ملفات أخرى</b>";
+            document.querySelectorAll("h4")[1].innerHTML = arabicH4;
+            document.querySelectorAll("h4")[1].classList.add("arabic"); // Add Arabic class
         }
     });
 });
