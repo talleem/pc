ffab54554c3d48196a2b91a142681c270d935e05
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-07 20:06:53 +0300

    Update pagewithtabs_AR.js

--- pagewithtabs_AR.js
+++ pagewithtabs_AR.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the localStorage variable `arpage` is set to 'ar'
+    if (localStorage.getItem("arpage") === "ar") {
+        // Translate the button texts into Arabic
+        const buttons = document.querySelectorAll('.tablink');
+        
+        if (buttons.length >= 7) {
+            buttons[0].innerHTML = "غرفة الاجتماعات G-meet";
+            buttons[1].innerHTML = "Reels مقاطع";
+            buttons[2].innerHTML = "Posts منشورات";
+            buttons[3].innerHTML = "Resumes سير ذاتية";
+            buttons[4].innerHTML = "lectures archive  محتوى";
+            buttons[5].innerHTML = "Admin مدير";
+            buttons[6].innerHTML = "sign out خروج";
+        }
+    }
+});
 
