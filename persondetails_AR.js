4ee9c8316de0ffc5edbb17487c9b315d885a3df8
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-08 21:33:45 +0300

    Update persondetails_AR.js

--- persondetails_AR.js
+++ persondetails_AR.js
@@ -1,1 +1,15 @@
+function translateToArabic() {
+    // Translate the table headers
+    document.querySelector('th:nth-child(1)').innerText = 'الأيميل';
+    document.querySelector('th:nth-child(2)').innerText = 'كلمة السر';
+    document.querySelector('th:nth-child(3)').innerText = 'الأسم';
+    document.querySelector('th:nth-child(4)').innerText = 'رقم البطاقة';
+    document.querySelector('th:nth-child(5)').innerText = 'رقم السجل';
+    document.querySelector('th:nth-child(6)').innerText = 'تاريخ الميلاد';
+    document.querySelector('th:nth-child(7)').innerText = 'الهاتف';
+    document.querySelector('th:nth-child(8)').innerText = 'هاتف الطواريء';
+    document.querySelector('th:nth-child(9)').innerText = 'العنوان';
+    document.querySelector('th:nth-child(10)').innerText = 'المدينة';
+    document.querySelector('th:nth-child(11)').innerText = 'المحاضرات';
+}
 
