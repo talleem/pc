e4b0943858184d9f6787349371196a11d0b6a86d
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-16 23:48:34 +0300

    Update videocompress.js

--- videocompress.js
+++ videocompress.js
@@ -1,5 +1,4 @@
 import { createFFmpeg, fetchFile } from 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@latest/dist/ffmpeg.min.js';
-import { FFmpeg } from 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@latest/dist/ffmpeg-core.min.js';
 
 // Initialize FFmpeg
 const ffmpeg = createFFmpeg({ log: true });
