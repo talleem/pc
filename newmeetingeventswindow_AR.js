7da622a9d50753cfd12a8a78454232010d84dba0
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-10-21 19:04:18 +0300

    Update newmeetingeventswindow_AR.js

--- newmeetingeventswindow_AR.js
+++ newmeetingeventswindow_AR.js
@@ -1,5 +1,5 @@
 // In updatemeetingwindow_AR.js
-function translateToArabic(updateWindow) {
+function translateToArabic_up(updateWindow) {
     const fieldTranslations = {
         'Description': 'ملاحظات',
         'Start Time': 'وقت البدء',
