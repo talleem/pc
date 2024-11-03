94316eb908e0745e3754a49b71237b9f6ee598e8
Hayder <108283167+engineerr1983@users.noreply.github.com>
2024-11-03 17:17:13 +0300

    Update gdrivefilerepl.js

--- gdrivefilerepl.js
+++ gdrivefilerepl.js
@@ -1,7 +1,8 @@
 // Function to populate table and check existence in Firestore
 function listFiles() {
     const accessToken = localStorage.getItem('accessToken');
-    const folderId = '1n7F6Dl6tGbw6lunDRDGYBNV-QThgJDer'; // Replace with actual folder ID
+    const folderId = '13Z8vqg6TPeeP4nbvaI1nDUmY8tRfuF6a'; // engineerr19832@gmail.com
+  //  const folderId = '1n7F6Dl6tGbw6lunDRDGYBNV-QThgJDer'; // engineerr1983@gmail.com
     const firestore = firebase.firestore(); // Ensure Firestore is initialized
 
     // Fetch files from Google Drive
